0813ca290cfa8e13e86517faedf093c4dfdabe60
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-06-29 16:04:10 +0100

    feat(tsup): Turn on sourcemaps in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export const tsup: Options = {
   dts: true,
   minify: true,
   splitting: true,
-  // sourcemap: true,
+  sourcemap: true,
   clean: true,
   format: ['esm', 'cjs'],
   entryPoints: ['src/index.ts', 'src/*.ts'],
